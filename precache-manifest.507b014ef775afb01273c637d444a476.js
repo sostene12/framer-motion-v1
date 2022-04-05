374344b65da199d007b3e254b8cf6f62de588515
sostene <ngarukiyimanasostene@gmail.com>
2022-04-05 09:24:10 +0200

    Updates

---
+++ precache-manifest.507b014ef775afb01273c637d444a476.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c72ed6c25abf5e1ab28c5a82549971",
+    "url": "/framer-motion-v1/index.html"
+  },
+  {
+    "revision": "5467be32998c8195f4b3",
+    "url": "/framer-motion-v1/static/css/main.13d0a309.chunk.css"
+  },
+  {
+    "revision": "957275e96d882548249f",
+    "url": "/framer-motion-v1/static/js/2.9cf8e510.chunk.js"
+  },
+  {
+    "revision": "ca72c7ec571e42135961743e973248e1",
+    "url": "/framer-motion-v1/static/js/2.9cf8e510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5467be32998c8195f4b3",
+    "url": "/framer-motion-v1/static/js/main.d915530b.chunk.js"
+  },
+  {
+    "revision": "104f8205f9baf6948917",
+    "url": "/framer-motion-v1/static/js/runtime-main.04ad9a14.js"
+  },
+  {
+    "revision": "630661b22ac5e7bb70d83757dc195958",
+    "url": "/framer-motion-v1/static/media/black.630661b2.png"
+  },
+  {
+    "revision": "4b46210e283e5b0cb191618b3c8eb8a5",
+    "url": "/framer-motion-v1/static/media/blue.4b46210e.png"
+  },
+  {
+    "revision": "671f7afd8afe7a8019c968a3f8e28ad3",
+    "url": "/framer-motion-v1/static/media/green.671f7afd.png"
+  },
+  {
+    "revision": "b12a7d0ec4a4c3aa47d8e454758907d4",
+    "url": "/framer-motion-v1/static/media/purp.b12a7d0e.png"
+  }
+]);
